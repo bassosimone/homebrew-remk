58338ee2f466b19aa59615704616f8b55770f889
Simone Basso <bassosimone@gmail.com>
2018-03-21 00:33:28 +0100

    libremk_platform: update version number

--- libremk_platform.rb
+++ libremk_platform.rb
@@ -1,7 +1,7 @@
 class LibremkPlatform < Formula
   desc "Really Experimental MK platform"
   homepage "https://measurement-kit.github.io/"
-  version "v1.0.0"
+  version "v1.0.1"
   url "https://github.com/bassosimone/libremk_platform/archive/v1.0.1.tar.gz"
   sha256 "9bd87b89424e9d60e395f19448569e244a55c18e776a1fff32231c816ef0e154"
 
