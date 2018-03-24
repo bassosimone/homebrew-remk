fa83129d3961df212bcde1ede4578aa0f4655d59
Simone Basso <bassosimone@gmail.com>
2018-03-24 17:41:23 +0100

    Build libremk_platform v2.0.0

--- libremk_platform.rb
+++ libremk_platform.rb
@@ -1,14 +1,13 @@
 class LibremkPlatform < Formula
   desc "Really Experimental MK platform"
   homepage "https://measurement-kit.github.io/"
-  version "v1.0.1"
-  url "https://github.com/bassosimone/libremk_platform/archive/v1.0.1.tar.gz"
-  sha256 "9bd87b89424e9d60e395f19448569e244a55c18e776a1fff32231c816ef0e154"
+  version "v2.0.0"
+  url "https://github.com/bassosimone/libremk_platform/archive/v2.0.0.tar.gz"
+  sha256 "8c0c3766164660d6b38840b3dffc4688a1d2083ea420fd87418c1b4f9abe00e4"
 
   depends_on "cmake"
 
   def install
-    system "./autogen.sh" if build.head?
     system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}",
            "-DBUILD_SHARED_LIBS=ON",
            "-DREMK_PLATFORM_BUILD_EXAMPLES=OFF"
