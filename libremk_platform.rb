e6822057cb8776c664084492773f509970c60516
Simone Basso <bassosimone@gmail.com>
2018-04-12 23:02:37 +0200

    Build v5.0.0

--- libremk_platform.rb
+++ libremk_platform.rb
@@ -1,9 +1,9 @@
 class LibremkPlatform < Formula
   desc "Really Experimental MK platform"
   homepage "https://measurement-kit.github.io/"
-  version "v4.0.0"
-  url "https://github.com/bassosimone/libremk_platform/archive/v4.0.0.tar.gz"
-  sha256 "65ae9b1070303177c5408e74acc8c3f1e0a286912f65f17c539d2b3caf4e95c6"
+  version "v5.0.0"
+  url "https://github.com/bassosimone/libremk_platform/archive/v5.0.0.tar.gz"
+  sha256 "92c1ce20e56f6249dd92ef942ef7517fb40edba0408c2f8970900c970603e6fe"
 
   depends_on "cmake"
 
